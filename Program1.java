ab2f146242283472aa7af021ed1e00a7b26596b8
Mohd Arif <31352655+mohdarif8299@users.noreply.github.com>
2019-04-04 18:16:18 +0530

    Add files via upload

--- Program1.java
+++ Program1.java
@@ -1,24 +1,13 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.FileNotFoundException;
-public class Program1
-{
-	public static void main(String[] args)  {
-	try (BufferedReader br = new BufferedReader(new FileReader("abc.txt"))) {
-		String s = br.readLine();
-		while (s!=null)
-		{
-			System.out.println(s);
-			s = br.readLine();
+import java.io.BufferedReader;
+import java.io.FileReader;
+public class Program1	{
+	public static void main(String[] args)	{
+		try(BufferedReader br = new BufferedReader(new FileReader("abc.txt")))	{
+			System.out.println(10/0);
 		}
-	}
-	catch(FileNotFoundException f) {
-		f.printStackTrace();
-	}
-	catch(IOException io) {
-		io.printStackTrace();
-	}
+		catch(ArithmeticException|Exception ie) {
+			ie.printStackTrace();
+		}
 	}
 }
